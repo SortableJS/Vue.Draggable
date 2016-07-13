1178d87219b9fa05b62d2fea1cb8bb7577eb3a78
pc-david\david.desmaisons <david.desmaisons@way2.com.br>
2016-07-13 14:38:00 -0300

    Fix reference to lodash (issue #3)

--- src/vuedragablefor.js
+++ src/vuedragablefor.js
@@ -61,7 +61,7 @@
 
   if (typeof exports == "object") {
     var _ = require("lodash");
-    var Sortable =  require("Sortable");
+    var Sortable =  require("Sortablejs");
     module.exports = buildVueDragFor(_, Sortable);
   } else if (typeof define == "function" && define.amd) {
     define(['lodash', 'Sortable'], function(_, Sortable){ return buildVueDragFor(_, Sortable);});
