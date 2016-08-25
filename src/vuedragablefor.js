9ad03c251651f16df210d52099cc703da41f257b
pc-david\david.desmaisons <david.desmaisons@way2.com.br>
2016-08-25 09:44:28 -0300

    fix typo+ case sensitive change for sortable dependency

--- src/vuedragablefor.js
+++ src/vuedragablefor.js
@@ -131,7 +131,9 @@
           }
         });
 
+        //With typo: should be removed in next release
         Vue.directive('dragable-for', dragableForDirective);
+        Vue.directive('draggable-for', dragableForDirective);
       }
     };
     return vueDragFor;
@@ -139,7 +141,7 @@
 
   if (typeof exports == "object") {
     var _ = require("lodash");
-    var Sortable =  require("Sortablejs");
+    var Sortable =  require("sortablejs");
     module.exports = buildVueDragFor(_, Sortable);
   } else if (typeof define == "function" && define.amd) {
     define(['lodash', 'Sortable'], function(_, Sortable){ return buildVueDragFor(_, Sortable);});
