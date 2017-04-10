71e003396e874b91461bc686ad560a6ba95484cb
pc-david\david.desmaisons <david.desmaisons@way2.com.br>
2017-04-10 18:20:07 -0300

    Adding new example

---
+++ examples/script/transition3.js
@@ -0,0 +1,37 @@
+var count =323;
+
+var vm = new Vue({
+  el: '#main',
+  name: 'mfgActivity',
+  data: {
+    jobs: [
+      {
+        jobNumber: "14037-12"
+      },
+      {
+        jobNumber: "14038-13"
+      },
+      {
+        jobNumber: "14048-15"
+      }
+    ],
+    jobs2: [
+      {
+        jobNumber: "14055-23"
+      },
+      {
+        jobNumber: "14089-10"
+      }
+    ],
+    drag: false
+  },
+  methods: {
+    removeJob: function (arr, index) {
+      // Remove job from GUI
+      arr.splice(index, 1);
+    },
+    add: function () {
+      this.jobs.push({jobNumber: "14037-"+ count++})
+    }
+  }
+})
