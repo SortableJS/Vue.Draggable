1f96a8412eedb6701dd9e025e9c34896f9a09eac
desmaisons_david <desmaisons_david@hotmail.com>
2017-05-11 15:05:06 -0300

    adding reactive disable example

--- dist/vuedraggable.js
+++ dist/vuedraggable.js
@@ -1,5 +1,4 @@
 'use strict';
-
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
@@ -335,7 +334,7 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
     return draggableComponent;
   }
 
-  if (typeof exports == "object") {
+if (typeof exports == "object") {
     var Sortable = require("sortablejs");
     module.exports = buildDraggable(Sortable);
   } else if (typeof define == "function" && define.amd) {
---
+++ examples/script/disable_2.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+  el: '#main',
+  data: {
+    items: ['1', '2', '3'],
+    options: {
+      disabled: false
+    }
+  }
+})
