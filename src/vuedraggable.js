38a86b120ddd3eb8dff09c1cffc1edc8c50310b5
dvdbot <dylan@vossystems.nl>
2017-11-10 11:20:19 +0100

    this._sortable undefined in beforeDestroy Hook
    There is an edge case in which this._sortable is undefined in the beforeDestroy Hook.
    This edge case can be seen when unmaximizing the screen to a size in which elements will be re-ordered when using bootstrap grid than maximizing and then minimizing again, now the error shows.
    
    This edit fixes this error but still after the first unmaximize after a refresh of the page the <draggable> element just disappears, still trying to find this issue

--- src/vuedraggable.js
+++ src/vuedraggable.js
@@ -128,7 +128,7 @@
       },
 
       beforeDestroy() {
-        this._sortable.destroy()
+        if(this._sortable !== null && this._sortable !== undefined) this._sortable.destroy();
       },
 
       computed: {
