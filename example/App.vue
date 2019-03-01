dc7ed7db62a5f458badeea65363d919849fd9267
desmaisons_david <desmaisons_david@hotmail.com>
2019-03-01 01:46:20 -0300

    Improving example

--- example/App.vue
+++ example/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="app">
+    <a href="https://github.com/SortableJS/Vue.Draggable" target="_blank">
+      <img
+        style="position: fixed; top: 0; right: 0; border: 0; z-index:99999"
+        width="149"
+        height="149"
+        src="https://github.blog/wp-content/uploads/2008/12/forkme_right_gray_6d6d6d.png?resize=149%2C149"
+        class="attachment-full size-full"
+        alt="Fork me on GitHub"
+        data-recalc-dims="1"
+      />
+    </a>
+
     <div class="logo">
-      <a href="https://github.com/SortableJS/Vue.Draggable" target="_blank"
-        ><img alt="Vue logo" src="./assets/logo.png"
-      /></a>
+      <img alt="Vue logo" src="./assets/logo.png" />
     </div>
 
     <div class="container ">
