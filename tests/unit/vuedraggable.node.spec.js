167f2986f52a47a8cd0e555c9820f17b69cfc3c7
desmaisons_david <desmaisons_david@hotmail.com>
2019-03-03 02:08:07 -0300

    Adding tests

--- tests/unit/vuedraggable.node.spec.js
+++ tests/unit/vuedraggable.node.spec.js
@@ -2,7 +2,26 @@
  * @jest-environment node
  */
 
-test('draggable can be required in a node environment', () => {
-  const draggable = require("@/vuedraggable");
-  expect(draggable).not.toBeNull();
+const Vue = require('vue');
+const renderer = require('vue-server-renderer').createRenderer();
+const draggable = require("@/vuedraggable").default;
+Vue.component('draggable', draggable);
+const app = new Vue({
+  name: "test-app",
+  template: `<draggable :list="items"><div v-for="item in items" :key="item"></div></draggable>`,
+  data:{
+    items:["a","b","c"]
+  }
 });
+
+let html;
+
+describe("vuedraggable in a SSR context", () => {
+  beforeEach(async () => {
+    html = await renderer.renderToString(app);
+  });
+
+  it("can be rendered", () => {
+    expect(html).not.toBeNull();
+  })
+})
