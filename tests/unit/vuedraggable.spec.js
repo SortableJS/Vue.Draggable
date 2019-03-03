5812dadcfbb9e29b2ed8eac4800597bb757627e0
desmaisons_david <desmaisons_david@hotmail.com>
2019-03-03 11:08:17 -0300

    Improving tests

--- tests/unit/vuedraggable.spec.js
+++ tests/unit/vuedraggable.spec.js
@@ -4,15 +4,28 @@ import draggable from "@/vuedraggable";
 let wrapper;
 let vm;
 let props;
+let items;
+let html;
 
 describe("draggable.vue", () => {
   beforeEach(() => {
-    wrapper = shallowMount(draggable);
+    items= ["a", "b", "c"];
+    wrapper = shallowMount(draggable,{
+      propsData:{
+        list: items
+      },
+      slots:{
+        default: items.map(item => `<div>${item}</div>`),
+        header: "<h1/>",
+        footer: "<footer/>"
+      }
+    });
     vm = wrapper.vm;
     props = vm.$options.props;
+    html = wrapper.element;
   });
 
-  it("should instanciate without error", () => {
+  it("should instantiate without error", () => {
     expect(wrapper).not.toBeUndefined();
   });
 
@@ -63,4 +76,23 @@ describe("draggable.vue", () => {
     expect(clone.type).toBe(Function);
     expect(clone.default(expected)).toBe(expected);
   })
+
+  it("renders correctly", () => {
+    expect(wrapper.html()).toMatch(/^<div>.*<\/div>$/);
+  })
+
+  test.each([
+    "ul",
+    "span",
+    "div"
+  ])(
+    "renders tag %s as root element",
+    (tag) => {
+      wrapper = shallowMount(draggable, {
+        propsData: { tag }
+      });
+      const expectedRegex = new RegExp(`^<${tag}>.*<\/${tag}>$`);
+      expect(wrapper.html()).toMatch(expectedRegex);
+    }
+  )
 });
