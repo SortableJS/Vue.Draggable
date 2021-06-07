76df629a427a2e05d09e34461b471cc168a1ca03
Sinyoung "Divinespear" Kang <divinespear@gmail.com>
2021-06-08 01:25:35 +0900

    add test for multi-select by multidrag plugin

--- tests/unit/vuedraggable.multidrag.spec.js
+++ tests/unit/vuedraggable.multidrag.spec.js
@@ -106,4 +106,100 @@ describe("draggable.vue with multidrag plugin", () => {
       expect(propsValue).toEqual(value);
     });
   });
+
+  describe("item select and deselect", () => {
+    let wrapper;
+    let vm;
+    const { addEventListener: originalAddEventListener, removeEventListener: originalRemoveEventListener } = document;
+
+    beforeEach(() => {
+      // event listener delegation hack
+      document.addEventListener = (type, listener) => {
+        originalAddEventListener.call(document, type, listener);
+        if (wrapper) {
+          wrapper.element.addEventListener(type, listener);
+        }
+      };
+      document.removeEventListener = (type, listener) => {
+        originalRemoveEventListener.call(document, type, listener);
+        if (wrapper) {
+          wrapper.element.removeEventListener(type, listener);
+        }
+      };
+      // component
+      const items = ["a", "b", "c", "d"];
+      const { wrapper: _w, vm: _v } = create({
+        propsData: {
+          list: items,
+          multiDrag: true,
+          selectedClass: 'selected',
+        },
+      });
+      wrapper = _w;
+      vm = _v;
+    });
+
+    afterEach(() => {
+      document.addEventListener = originalAddEventListener;
+      document.removeEventListener = originalRemoveEventListener;
+    });
+
+    it("should be selected", async () => {
+      const wrapperItems = wrapper.findAll('.item');
+      const item1 = wrapperItems.at(0);
+      const item2 = wrapperItems.at(wrapperItems.length - 1);
+
+      expect(item1.element.matches('.selected')).toBe(false);
+      expect(item2.element.matches('.selected')).toBe(false);
+
+      // select first item
+      // simulate mouse event
+      await item1.trigger('mousedown').then(() => item1.trigger('mouseup'));
+      expect(item1.element.matches('.selected')).toBe(true);
+
+      // select second item
+      // simulate mouse event
+      await item2.trigger('mousedown').then(() => item2.trigger('mouseup'));
+      expect(item2.element.matches('.selected')).toBe(true);
+
+      // check emit event
+      const { select: selectEmit } = wrapper.emitted();
+      expect(selectEmit).not.toBeUndefined();
+      expect(selectEmit).toHaveLength(2);
+      const [[emitEvent1], [emitEvent2]] = selectEmit;
+      expect(emitEvent1.items).toEqual([item1.element]);
+      expect(emitEvent2.items).toEqual([item1.element, item2.element]);
+    });
+
+    it("should be deselected", async () => {
+      const wrapperItems = wrapper.findAll('.item');
+      const item1 = wrapperItems.at(0);
+      const item2 = wrapperItems.at(wrapperItems.length - 1);
+
+      // select items for deselect
+      Sortable.utils.select(item1.element);
+      Sortable.utils.select(item2.element);
+
+      expect(item1.element.matches('.selected')).toBe(true);
+      expect(item2.element.matches('.selected')).toBe(true);
+
+      // deselect first item
+      // simulate mouse event
+      await item1.trigger('mousedown').then(() => item1.trigger('mouseup'));
+      expect(item1.element.matches('.selected')).toBe(false);
+
+      // deselect second item
+      // simulate mouse event
+      await item2.trigger('mousedown').then(() => item2.trigger('mouseup'));
+      expect(item2.element.matches('.selected')).toBe(false);
+
+      // check emit event
+      const { deselect: deselectEmit } = wrapper.emitted();
+      expect(deselectEmit).not.toBeUndefined();
+      expect(deselectEmit).toHaveLength(2);
+      const [[emitEvent1], [emitEvent2]] = deselectEmit;
+      expect(emitEvent1.items).toEqual([item2.element]);
+      expect(emitEvent2.items).toEqual([]);
+    });
+  });
 });
